a41c14decda1069a56c100fe020550c0f78878d6
PatnaikSarthak <127627723+PatnaikSarthak@users.noreply.github.com>
2023-06-06 05:37:59 +0530

    Update 10.5.js

--- Week-10/10.5.js
+++ Week-10/10.5.js
@@ -1,33 +1,25 @@
 //Program to find the town judge
-let vertices;
-let AdjMatrix=[];
-function graph(v){
-    for(let i=0;i<v;i++){
-        AdjMatrix[i]=[];
-    }
-}
-function townJudge(){
+function townJudge(n, trust){
     var judgeFound=-1;
-    for(let i=0;i<AdjMatrix.length;i++){
-        if(AdjMatrix[i].length==0){
-            var temp = i;
-            var indicator =true;
-            for(let j=0; j<AdjMatrix.length;j++){
-                if(AdjMatrix[j].indexOf(temp)==-1 && temp!=j){
-                    indicator=false;
-                    break;
-                }
-            }
-            if (indicator){
-                judgeFound=temp;
-            }
+    var trustsOtherPerson = new Array(n);
+    var countOfPeopleTrusting = new Array(n);
+    for(let i=0; i<n; i++){
+        trustsOtherPerson[i]=0;
+        countOfPeopleTrusting[i]=0;
+    }
+    for(let [i,j] in trust){
+        trustsOtherPerso[i]=1;
+        countOfPeopleTrusting[j]+=1;
+    }
+    for(let i=0;i<n;i++){
+        if(trustsOtherPerson[i]==0 && countOfPeopleTrusting[i==n-1]){
+            judgeFound=i;
         }
     }
     return judgeFound;
 }
 graph(4);
 var trust = [[0,2],[1,2],[3,2],[0,1],[1,3]];
-for(let i=0; i<trust.length; i++){
-    AdjMatrix[trust[i][0]].push(trust[i][1]);
-}
-console.log(townJudge());
+console.log(townJudge(4,trust));
+//Time complexity: O(n)
+//Space complexity: O(n)
